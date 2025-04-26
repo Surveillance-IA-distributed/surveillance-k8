a16ddf0945b5120f19fe1f1fe2bbf6ee23d67ad4
Brigham Caceres <brigham.caceres@ucsp.edu.pe>
2025-04-25 19:26:12 -0500

    fix reset

--- reset_services.sh
+++ reset_services.sh
@@ -11,8 +11,7 @@ echo "=== [3/6] Eliminando configuraciones de red y datos ==="
 sudo iptables -F && sudo iptables -t nat -F && sudo iptables -t mangle -F && sudo iptables -X
 sudo ipvsadm --clear 2>/dev/null
 sudo rm -rf ~/.kube
-sudo rm -rf /etc/cni /etc/kubernetes /var/lib/etcd /var/lib/cni /var/lib/kubelet /opt/cni
-
+sudo rm -rf /etc/cni /etc/kubernetes /var/lib/etcd 
 echo "=== [4/6] Reiniciando el contenedor y kubelet ==="
 sudo systemctl daemon-reexec
 sudo systemctl restart docker || sudo systemctl restart containerd
